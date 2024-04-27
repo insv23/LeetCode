444745fe6764e41e094fefc2cf814ff11062ffc9
insv <insv23@outlook.com>
2024-04-27 11:43:35 +0800

    T3: second

--- 3.Longest Substring Without Repeating Characters/b.SlidingWindow.py
+++ 3.Longest Substring Without Repeating Characters/b.SlidingWindow.py
@@ -11,8 +11,8 @@ class Solution(object):
         for right in range(len(s)):
             if s[right] in char_index_map:
             # 当遇见重复字符，需要更新 left
-                # left 只能往右，例如 `abbbba`
-                left = max(left, char_index_map[s[right]] + 1)
+                # left 只能往右，例如 `abbbba`, `tmmzuxt`
+                left = max(left, char_index_map[s[right]] + 1) # 这个左侧的更新条件非常微妙, 'r 所在字符出现过' 有两种可能: 1. 在当前'最长子串'中出现, 则更新 l 到下一个位置 2. 未在当前'最长子串'中出现(即在 l 的左侧出现, 例如 `tmmzuxt` 中的 `t`), 则 l 应保持不变
             # 更新字符(无论是不是遇到过)索引
             char_index_map[s[right]] = right
             # 更新最大长度
